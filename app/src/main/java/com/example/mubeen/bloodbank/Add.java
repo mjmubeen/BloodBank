1e02343b7015dda50f35111f08e5065b18c20b3c
Mubeen Javed <mjmubeen123@gmail.com>
2018-12-03 23:54:19 +0500

    Rename Home to Add and added intro.java and activity_intro.xml

--- app/src/main/java/com/example/mubeen/bloodbank/Add.java
+++ app/src/main/java/com/example/mubeen/bloodbank/Add.java
@@ -1,10 +1,7 @@
 package com.example.mubeen.bloodbank;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 public class Add extends AppCompatActivity {
 
