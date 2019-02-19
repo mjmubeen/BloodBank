901235ec100a38c6c0a540fb2384ce183df5aa6f
Mubeen Javed <mjmubeen123@gmail.com>
2019-02-19 13:54:14 +0500

    Mubeen

--- app/src/main/java/com/example/mubeen/bloodbank/NavigationDrawerActivity.java
+++ app/src/main/java/com/example/mubeen/bloodbank/NavigationDrawerActivity.java
@@ -58,7 +58,7 @@ public class NavigationDrawerActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.logout) {
             return true;
         }
 
