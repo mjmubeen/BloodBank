04e73605a0211dd70f2c1709fefa6bc08c9e7a75
Mubeen Javed <mjmubeen123@gmail.com>
2019-02-19 12:55:30 +0500

    Mubeen

--- app/src/main/java/com/example/mubeen/bloodbank/CustomAdapter.java
+++ app/src/main/java/com/example/mubeen/bloodbank/CustomAdapter.java
@@ -5,7 +5,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -40,7 +39,6 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.donar_info_layout, parent, false);
-
         return new MyViewHolder(view);
     }
 
