faae05fba94b842dd26a844b89169b84e9cf2985
CH <danyal@ucp.edu.pk>
2019-02-20 10:47:18 +0500

    Danyal

---
+++ app/src/main/java/com/example/mubeen/bloodbank/BloodBanks.java
@@ -0,0 +1,9 @@
+package com.example.mubeen.bloodbank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+
+public class BloodBanks extends AppCompatActivity
+{
+    RecyclerView recyclerView;
+}
